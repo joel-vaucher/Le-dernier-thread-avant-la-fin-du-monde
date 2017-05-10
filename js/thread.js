7fd317f89a5fd9c9a408f288b98593487a1adc57
julien-baumgartner <julien.baumgartner@he-arc.ch>
2017-05-10 17:31:04 +0200

    cases dans thread

--- js/thread.js
+++ js/thread.js
@@ -1,9 +1,11 @@
 class Thread{
+	
 	constructor(x, canvas) {
 		this.canvas = canvas;
 		this.x = x;
 		this.threadY = 20;
 		this.color = '#000055';
+		this.content = ["case1","case2","case3","case4"];
 	  }
 	  
 	 draw(){
@@ -15,16 +17,36 @@ class Thread{
         fromCenter: false
 		});
 		
+		
+		for(var i = 1; i <= this.content.length; i++){
+			this.canvas.drawRect({
+			fillStyle: '#000000',
+			x:this.x + 5, y: i * 150,
+			width: 30,
+			height: 20,
+			fromCenter: true
+			});
+		}
+	 
+	 
 		this.canvas.drawArc({
         fillStyle: this.color,
         x:this.x + 5, y: this.threadY,
         radius: 25,
         fromCenter: true
 		});
-	 
 	 }
 	 
 	 move(){
+		 if(this.threadY % 150 == 0 && this.threadY > 0){
+			 var index = this.threadY/150 - 1;
+			 if(index >= this.content.length){
+				alert("end");
+				this.threadY = 0;
+			 }else{
+				alert(this.content[index]);
+			 }
+		 }
 		 this.threadY++;
 	 }
 }
